fa62cf8022fc8b560a45518a68d2426698e6e697
JuliyaSavelyeva <juliyasavelyeva0605@gmail.com>
2021-06-25 14:38:34 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const rootElement = document.querySelector('#root');
+
+const elem = <h1 className="title">Todo List</h1>;
+
+ReactDOM.render(elem, rootElement);
